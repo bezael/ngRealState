1601c053967dff4e940e168ecf31a9e2515e28b1
Bezael Pérez <bezael_e@hotmail.com>
2018-01-03 13:22:23 +0100

    Add some new component, and new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,31 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { CribsService } from './services/cribs.service';
 import { UtilService } from './services/util.service';
 import { AddListingFormComponent } from './components/add-listing-form/add-listing-form.component';
 import { SortByPipe } from './pipes/sort-by/sort-by.pipe';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'signin',     component: SignInComponent },
+  { path: 'register',  component: RegisterUserComponent },
+  { path: 'contact',  component: ContactPageComponent },
+  { path: 'blog',  component: BlogPageComponent },
+  {path: 'about', component: AboutPageComponent }
+
+];
 
 @NgModule({
   declarations: [
@@ -27,9 +47,19 @@ import { HeroSectionComponent } from './components/hero-section/hero-section.com
     CribCardComponent,
     AddListingFormComponent,
     SortByPipe,
-    HeroSectionComponent
+    HeroSectionComponent,
+    SignInComponent,
+    RegisterUserComponent,
+    ContactPageComponent,
+    BlogPageComponent,
+    AboutPageComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,
+      {enableTracing: true }  
+    ),
     BrowserModule,
     HttpModule,
     FormsModule,
